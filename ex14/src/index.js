d920beb205d5e5d9d65de9ac49fadaaae199915b
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-12 12:56:25 +0100

    Solution for the example 14

---
+++ ex14/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code below this line
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+
+    var wordBlanks = "My " + myNoun + " and my cat " + myVerb + " into a " + myAdjective + " crowd" + " very " + myAdverb + ".";
+    // Only change code above this line
+    
+    return wordBlanks;
+    
+    }
+    console.log(main());
+    module.exports = main;
